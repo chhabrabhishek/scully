d8f2552c242a4c837c1989a26944808ba17a221b
Abhishek Chhabra <abhishekchhabra@Abhisheks-MacBook-Air.local>
2022-05-02 17:22:36 +0530

    get the transactions from graphql api using flask

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -1,50 +1,21 @@
+import { useEffect, useState } from 'react';
 import Transaction from '../Transaction/Transaction';
 import './TransactionList.css';
 
 const TransactionList = () => {
 
-    const transactionList = [
-        {
-            transactionId: '1',
-            transactionType: 'lends',
-            transactionDate: '2nd May 2022',
-            transactionStatus: 'unpaid',
-            transactionFrom: 'abhishekchhabra',
-            transactionWith: 'anujchhabra',
-            reason: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, dignissimos cum, natus nulla quidem, sint iusto quam accusamus explicabo nobis quisquam. Dolor itaque aperiam, quia minus ratione fuga accusantium voluptates.',
-            money: '2500'
-        },
-        {
-            transactionId: '2',
-            transactionType: 'borrows',
-            transactionDate: '22nd April 2022',
-            transactionStatus: 'paid',
-            transactionFrom: 'abhishekchhabra',
-            transactionWith: 'anujchhabra',
-            reason: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, dignissimos cum, natus nulla quidem, sint iusto quam accusamus explicabo nobis quisquam. Dolor itaque aperiam, quia minus ratione fuga accusantium voluptates.',
-            money: '1500'
-        },
-        {
-            transactionId: '3',
-            transactionType: 'borrows',
-            transactionDate: '1st April 2022',
-            transactionStatus: 'paid',
-            transactionFrom: 'abhishekchhabra',
-            transactionWith: 'anujchhabra',
-            reason: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, dignissimos cum, natus nulla quidem, sint iusto quam accusamus explicabo nobis quisquam. Dolor itaque aperiam, quia minus ratione fuga accusantium voluptates.',
-            money: '3500'
-        },
-        {
-            transactionId: '4',
-            transactionType: 'lends',
-            transactionDate: '2nd February 2022',
-            transactionStatus: 'paid',
-            transactionFrom: 'abhishekchhabra',
-            transactionWith: 'anujchhabra',
-            reason: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, dignissimos cum, natus nulla quidem, sint iusto quam accusamus explicabo nobis quisquam. Dolor itaque aperiam, quia minus ratione fuga accusantium voluptates.',
-            money: '2000'
-        }
-    ]
+    const [transactionList, setTransactionList] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/getTransactions', {
+            method: 'GET'
+        })
+        .then(response => {
+            response.json().then(data => {
+                setTransactionList(data.data.transactions);
+            })
+        })
+    }, [])
 
     return (
         <div className='transactionListParent'>
